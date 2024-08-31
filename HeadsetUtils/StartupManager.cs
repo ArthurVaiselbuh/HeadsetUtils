98462f910ee34ea33f96ddc7e60517939229c4ae
Arthur <arthurv@mail.tau.ac.il>
2024-08-31 21:05:29 +0300

    Fix incorrect value passed to  delete startup key

--- HeadsetUtils/StartupManager.cs
+++ HeadsetUtils/StartupManager.cs
@@ -22,7 +22,7 @@ namespace HeadsetUtils
             }
             else
             {
-                key.DeleteValue(startupKey);
+                key.DeleteValue(programName);
             }
         }
     }
